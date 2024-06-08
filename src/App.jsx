1589f8cbb651236acc4df10414720c4a10e5f212
CasManny <chukwusabastine2018@gmail.com>
2024-06-08 07:41:55 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+      <Header />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
